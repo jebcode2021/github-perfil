e1d07f05177ba73355d855defa55c7f2fc1ef830
jebcode2021 <jebcode@outlook.com.br>
2024-01-08 15:36:30 -0300

    Desafio

--- src/components/perfil/index.jsx
+++ src/components/perfil/index.jsx
@@ -6,6 +6,7 @@ const Perfil = ({ nomeUsuario }) => {
         <header className={style.header}>
             <img className={style.avatar} src={`https://github.com/${nomeUsuario}.png`} />
             <h1 className={style.name}>{nomeUsuario}</h1>
+            <a target='_blank' className={style.link} href={`https://github.com/${nomeUsuario}`} target="_blank" rel="noreferrer"> visitar perfil </a>
         </header>
     )
 }
